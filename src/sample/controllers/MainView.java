9df56d151c699fbf53be59a020bc6eb54712d34d
Sebastian Strzalkowski <sebastianstrzalkowski.strzala@gmail.com>
2018-03-18 15:44:18 +0100

    bar values

--- src/sample/controllers/MainView.java
+++ src/sample/controllers/MainView.java
@@ -1,33 +1,21 @@
 package sample.controllers;
 
-import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import pl.waw.wawvote.utils.MultichainAPI;
+import javafx.scene.text.Text;
 import sample.utils.MultichainConnection;
-import sample.utils.MultichainConnectionI;
 
-import javax.management.Notification;
-import java.io.IOException;
 import java.net.URL;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 public class MainView implements Initializable {
 
@@ -50,7 +38,12 @@ public class MainView implements Initializable {
             new String("Hanna Gronkiewicz-Waltz")
 
     );
-
+    public Text textCandidate1;
+    public Text textCandidate2;
+    public Text textCandidate3;
+    public Text textCandidate4;
+    public Text textCandidate5;
+    public Text textCandidate6;
 
 
     public void initialize(URL location, ResourceBundle resources) {
@@ -82,12 +75,11 @@ public class MainView implements Initializable {
         multichainConnection.vote(candidate);
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText("Udało się zagłosować");
+        alert.setHeaderText("Dziękujemy za oddany głos");
         alert.showAndWait();
     }
 
     public void checkResult(ActionEvent actionEvent) {
-
 
         Map<Integer, Long> counters = multichainConnection.getResults();
 
@@ -101,6 +93,28 @@ public class MainView implements Initializable {
         series1.getData().add(new XYChart.Data(candidateList.get(3), counters.containsKey(3)?counters.get(3):0));
         series1.getData().add(new XYChart.Data(candidateList.get(4), counters.containsKey(4)?counters.get(4):0));
         series1.getData().add(new XYChart.Data(candidateList.get(5), counters.containsKey(5)?counters.get(5):0));
+
+        XYChart.Data<Integer, Long> data = new XYChart.Data(candidateList.get(0), counters.containsKey(0)?counters.get(0):0);
+        textCandidate1.setText(String.valueOf(data.getYValue()));
+
+
+        data = new XYChart.Data(candidateList.get(1), counters.containsKey(1)?counters.get(1):0);
+        textCandidate2.setText(String.valueOf(data.getYValue()));
+
+        data = new XYChart.Data(candidateList.get(2), counters.containsKey(2)?counters.get(2):0);
+        textCandidate3.setText(String.valueOf(data.getYValue()));
+
+        data = new XYChart.Data(candidateList.get(3), counters.containsKey(3)?counters.get(3):0);
+        textCandidate4.setText(String.valueOf(data.getYValue()));
+
+        data = new XYChart.Data(candidateList.get(4), counters.containsKey(4)?counters.get(4):0);
+        textCandidate5.setText(String.valueOf(data.getYValue()));
+
+        data = new XYChart.Data(candidateList.get(5), counters.containsKey(5)?counters.get(5):0);
+        textCandidate6.setText(String.valueOf(data.getYValue()));
+
         chartResult.getData().setAll(series1);
     }
 }
+
+
