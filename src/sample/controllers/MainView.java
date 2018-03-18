231b9c23fcded7bbd549e8bb163ae6277179a962
Sebastian Strzalkowski <sebastianstrzalkowski.strzala@gmail.com>
2018-03-18 12:49:40 +0100

    chart fix v2

--- src/sample/controllers/MainView.java
+++ src/sample/controllers/MainView.java
@@ -17,26 +17,29 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import pl.waw.wawvote.utils.MultichainAPI;
+import sample.utils.MultichainConnection;
 import sample.utils.MultichainConnectionI;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainView implements Initializable {
 
 
 
 
-    private ComboBox candidateBox = new ComboBox();
+    public ComboBox candidateBox = new ComboBox();
     public BarChart<String, Integer> chartResult;
-    private MultichainAPI multichainAPI;
-    private XYChart.Series series1 = new XYChart.Series();
-    private final CategoryAxis xAxis = new CategoryAxis();
-    private final NumberAxis yAxis = new NumberAxis();
+    public MultichainAPI multichainAPI;
+    public XYChart.Series series1 = new XYChart.Series();
+    public final CategoryAxis xAxis = new CategoryAxis();
+    public final NumberAxis yAxis = new NumberAxis();
 
-    private ObservableList<String> candidateList = FXCollections.observableArrayList(
+    public ObservableList<String> candidateList = FXCollections.observableArrayList(
             new String("Paweł Rabiej"),
             new String("Rafał Trzaskowski"),
             new String("Piotr Lorey-Marzec"),
@@ -47,10 +50,11 @@ public class MainView implements Initializable {
     );
 
 
+
     public void initialize(URL location, ResourceBundle resources) {
 
 
-        //multichainAPI = new MultichainAPI();
+        multichainAPI = new MultichainAPI();
 
         xAxis.setLabel("Kandydat");
         yAxis.setLabel("Ilość głosów");
@@ -65,26 +69,31 @@ public class MainView implements Initializable {
         series1.getData().add(new XYChart.Data(candidateList.get(5), 0));
 
         chartResult.getData().addAll(series1);
-
+        candidateBox.getItems().addAll(candidateList);
 
     }
 
     public void voteButton(ActionEvent actionEvent) {
 
-        //int candidate = candidateBox.getSelectionModel().getSelectedIndex();
-        //multichainAPI.addToChain("stream1", "key1", candidate);
+        int candidate = candidateBox.getSelectionModel().getSelectedIndex();
+        System.out.println(candidate);
+        multichainAPI.addToChain("stream1", "key1", candidate);
 
     }
 
     public void checkResult(ActionEvent actionEvent) {
 
+        MultichainConnection multichainConnection = new MultichainConnection();
+        Map<Integer, Long> counters = multichainConnection.getResults();
+
+        System.out.println(counters);
         series1.setName("2018");
-        series1.getData().add(new XYChart.Data(candidateList.get(0), 30));
-        series1.getData().add(new XYChart.Data(candidateList.get(1), 20));
-        series1.getData().add(new XYChart.Data(candidateList.get(2), 11));
-        series1.getData().add(new XYChart.Data(candidateList.get(3), 5));
-        series1.getData().add(new XYChart.Data(candidateList.get(4), 17));
-        series1.getData().add(new XYChart.Data(candidateList.get(5), 17));
+        series1.getData().add(new XYChart.Data(candidateList.get(0), counters.get(0)));
+        series1.getData().add(new XYChart.Data(candidateList.get(1), counters.get(1)));
+        series1.getData().add(new XYChart.Data(candidateList.get(2), counters.containsKey(2)?counters.get(2):0));
+        series1.getData().add(new XYChart.Data(candidateList.get(3), counters.get(3)));
+        series1.getData().add(new XYChart.Data(candidateList.get(4), counters.get(4)));
+        series1.getData().add(new XYChart.Data(candidateList.get(5), counters.get(5)));
         chartResult.getData().setAll(series1);
     }
 }
