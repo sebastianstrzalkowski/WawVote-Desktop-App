47a0611c7dc5b27d64f712d0787d1e38b1ae0032
Sebastian Strzalkowski <sebastianstrzalkowski.strzala@gmail.com>
2018-03-18 10:52:12 +0100

    chart add

--- src/sample/controllers/MainView.java
+++ src/sample/controllers/MainView.java
@@ -7,20 +7,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sample.utils.MultichainConnectionI;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MainView implements Initializable {
 
 
+
+
     public ComboBox candidateBox = new ComboBox();
+    public BarChart chartResult;
 
     private ObservableList<String> candidateList = FXCollections.observableArrayList(
             new String("Paweł Rabiej"),
@@ -35,14 +44,32 @@ public class MainView implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
 
-
-
-
         candidateBox.getItems().addAll(candidateList );
+
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> chartResult =
+                new BarChart<String,Number>(xAxis,yAxis);
+        chartResult.setTitle("Country Summary");
+        xAxis.setLabel("Kandydaci");
+        yAxis.setLabel("Ilość głosów");
 
     }
 
     public void voteButton(ActionEvent actionEvent) {
     }
 
+    public void checkResult(ActionEvent actionEvent) {
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2003");
+        series1.getData().add(new XYChart.Data(candidateList.get(0), 30));
+        series1.getData().add(new XYChart.Data(candidateList.get(1), 20));
+        series1.getData().add(new XYChart.Data(candidateList.get(2), 11));
+        series1.getData().add(new XYChart.Data(candidateList.get(3), 5));
+        series1.getData().add(new XYChart.Data(candidateList.get(4), 17));
+        series1.getData().add(new XYChart.Data(candidateList.get(5), 17));
+        chartResult.getData().addAll(series1);
+    }
 }
